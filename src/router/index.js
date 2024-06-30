14dc8c3c17f4010187a3b5432b26bf3d01c42127
HULER-local <2799591178@qq.com>
2024-06-30 15:43:09 +0800

    打包本地启动

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import {createRouter,createWebHistory} from "vue-router";
+import {createRouter, createWebHashHistory} from "vue-router";
 import Login from "@/components/LogView.vue";
 import Register from "@/components/RegView.vue";
 import MainPage from "@/components/MainPage.vue";
@@ -15,7 +15,7 @@ const routes=[
 ]
 
 const router= createRouter({
-    history: createWebHistory(),
+    history: createWebHashHistory(),
     routes: routes,
 })
 
